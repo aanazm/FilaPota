ad1014dd568a7a900754fb81a7bcfd06f595ab84
aanazm <aan.azm97@gmail.com>
2024-07-16 11:02:53 +0700

    client side layout view: Order, category, detail, auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\HomePage;
+use App\Livewire\MyOrderPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\ProductDetailPage;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\SuccessPage;
+use Filament\Pages\Auth\Register;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +28,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
+
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrderPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forget', ForgotPasswordPage::class);
+Route::get('/reset', ResetPasswordPage::class);
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
+
+
+
